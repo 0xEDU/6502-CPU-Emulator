ffe18939f6791d0649f1d0a49096de24455ce198
edu <bfwaend@gmail.com>
2023-08-25 00:53:19 -0300

    test: add LDAZeroPageCanLoadAValueIntoTheARegister test

--- src/tests.cpp
+++ src/tests.cpp
@@ -28,6 +28,21 @@ TEST_F(M6502Test1, LDAImmediateCanLoadAValueIntoTheARegister) {
     EXPECT_EQ(cpu.A, 0x84);
 }
 
+TEST_F(M6502Test1, LDAZeroPageCanLoadAValueIntoTheARegister) {
+    // Given:
+    // start - inline a small program
+    mem[0xFFFC] = CPU::INS_LDA_ZP;
+    mem[0xFFFD] = 0x42;
+    mem[0x0042] = 0x37;
+    // end - inline a small program
+
+    // When:
+    cpu.execute(3, mem);
+
+    // Then:
+    EXPECT_EQ(cpu.A, 0x37);
+}
+
 #if 0
 int main() {
 
