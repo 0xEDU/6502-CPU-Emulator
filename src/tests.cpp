0e903f7718ba9a6a6d6e2cc8f0189f7e312209ac
edu <bfwaend@gmail.com>
2023-08-25 20:03:34 -0300

    tests: add check for cpu status

--- src/tests.cpp
+++ src/tests.cpp
@@ -22,10 +22,18 @@ TEST_F(M6502Test1, LDAImmediateCanLoadAValueIntoTheARegister) {
     // end - inline a small program
 
     // When:
+    CPU cpuCopy = cpu;
     cpu.execute(2, mem);
 
     // Then:
     EXPECT_EQ(cpu.A, 0x84);
+    EXPECT_FALSE(cpu.Z);
+    EXPECT_TRUE(cpu.N);
+    EXPECT_EQ(cpu.C, cpuCopy.C);
+    EXPECT_EQ(cpu.I, cpuCopy.I);
+    EXPECT_EQ(cpu.D, cpuCopy.D);
+    EXPECT_EQ(cpu.B, cpuCopy.B);
+    EXPECT_EQ(cpu.V, cpuCopy.V);
 }
 
 TEST_F(M6502Test1, LDAZeroPageCanLoadAValueIntoTheARegister) {
@@ -37,10 +45,18 @@ TEST_F(M6502Test1, LDAZeroPageCanLoadAValueIntoTheARegister) {
     // end - inline a small program
 
     // When:
+    CPU cpuCopy = cpu;
     cpu.execute(3, mem);
 
     // Then:
     EXPECT_EQ(cpu.A, 0x37);
+    EXPECT_FALSE(cpu.Z);
+    EXPECT_FALSE(cpu.N);
+    EXPECT_EQ(cpu.C, cpuCopy.C);
+    EXPECT_EQ(cpu.I, cpuCopy.I);
+    EXPECT_EQ(cpu.D, cpuCopy.D);
+    EXPECT_EQ(cpu.B, cpuCopy.B);
+    EXPECT_EQ(cpu.V, cpuCopy.V);
 }
 
 TEST_F(M6502Test1, LDAZeroPageXCanLoadAValueIntoTheARegister) {
@@ -53,10 +69,18 @@ TEST_F(M6502Test1, LDAZeroPageXCanLoadAValueIntoTheARegister) {
     // end - inline a small program
 
     // When:
+    CPU cpuCopy = cpu;
     cpu.execute(4, mem);
 
     // Then:
     EXPECT_EQ(cpu.A, 0x37);
+    EXPECT_FALSE(cpu.Z);
+    EXPECT_FALSE(cpu.N);
+    EXPECT_EQ(cpu.C, cpuCopy.C);
+    EXPECT_EQ(cpu.I, cpuCopy.I);
+    EXPECT_EQ(cpu.D, cpuCopy.D);
+    EXPECT_EQ(cpu.B, cpuCopy.B);
+    EXPECT_EQ(cpu.V, cpuCopy.V);
 }
 
 TEST_F(M6502Test1, LDAZeroPageXCanLoadAValueIntoTheARegisterWhenItWraps) {
