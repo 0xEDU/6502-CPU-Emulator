a1e2d7e961d028511aea4b64737a9fc6514cb7ff
edu <bfwaend@gmail.com>
2023-08-25 00:36:34 -0300

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ DIR=build
 
 mkdir -p $DIR
 echo -e "Building..."
-cmake --build $DIR
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -B $DIR
+cp $DIR/compile_commands.json .
 echo -e "Running make..."
 make --no-print-dir -C $DIR -j4
