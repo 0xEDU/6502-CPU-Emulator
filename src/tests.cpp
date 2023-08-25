aca9edd3e864b8400775eac8d4cec42a738facf1
edu <bfwaend@gmail.com>
2023-08-25 00:50:30 -0300

    test: add first test for LDA

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,19 +1,36 @@
 #include "../includes/6502cpu.hpp"
+#include <gtest/gtest.h>
 
-#if 0
-int main() {
+class M6502Test1 : public testing::Test  {
+    protected:
+
     Mem mem;
     CPU cpu;
 
-    cpu.reset(mem);
+    void SetUp() override {
+        cpu.reset(mem);
+    }
+
+    void TearDown() override {}
+};
+
+TEST_F(M6502Test1, LDAImmediateCanLoadAValueIntoTheARegister) {
+    // Given:
     // start - inline a small program
-    mem[0xFFFC] = CPU::INS_JSR;
-    mem[0xFFFD] = 0x42;
-    mem[0xFFFE] = 0x42;
-    mem[0x4242] = CPU::INS_LDA_IM;
-    mem[0x4243] = 0x84;
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x84;
     // end - inline a small program
-    cpu.execute(9, mem);
+
+    // When:
+    cpu.execute(2, mem);
+
+    // Then:
+    EXPECT_EQ(cpu.A, 0x84);
+}
+
+#if 0
+int main() {
+
     return 0;
 }
 #endif
